a3d1ad10fbf633524a74cea9de478729c535167f
kwaleyela-ikafa <kwaleyelamusil@gmail.com>
2022-12-12 23:49:09 +0200

    Add Foodlist

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,18 @@ Rails.application.routes.draw do
 
   devise_scope :user do
     authenticated :user do
-      root :to => 'users#index', as: :authenticated_root
+      root :to => 'foods#index', as: :authenticated_root
     end
     unauthenticated :user do
       root :to => 'devise/registrations#new', as: :unauthenticated_root
     end
   end
 
-  resources :users
+  resources :users do
+    resources :foods
+  end
+
+  # end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
