1647f0341240467ad3d49c181ad4a538da49d1de
kwaleyela-ikafa <kwaleyelamusil@gmail.com>
2022-12-12 13:34:04 +0200

    Add-Login-page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  devise_for :users
+
+  devise_scope :user do
+    authenticated :user do
+      root :to => 'users#index', as: :authenticated_root
+    end
+    unauthenticated :user do
+      root :to => 'devise/registrations#new', as: :unauthenticated_root
+    end
+  end
+
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
